98ca34234fa2a1003a8f9b8404cb0edec42f2104
Rob Lohmann <roblohman@gmail.com>
2019-10-01 11:58:46 +0200

    added initial project with requesthelper

---
+++ CodeCube.API/RequestHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CodeCube.API
+{
+    public static class RequestHelper
+    {
+        /// <summary>
+        /// Get a <see cref="StringContent"/> object to use with an API-request. 
+        /// </summary>
+        /// <param name="requestObject">The object to serialize to <see cref="StringContent"/></param>
+        /// <returns>Stringcontent object with UTF-8 encoding and application/json as mediatype.</returns>
+        public static StringContent GetRequestContent(object requestObject)
+        {
+            return new StringContent(JsonConvert.SerializeObject(requestObject), Encoding.UTF8, "application/json");
+        }
+    }
+}
